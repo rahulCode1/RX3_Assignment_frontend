f1b6e0d2eb9baecd52177d1d4b052728ec67b513
Rahul <pbrkumawat@gmail.com>
2026-01-29 19:24:45 +0530

    Char: Add loading feature text on classes & schools component

--- src/school/SchoolView.jsx
+++ src/school/SchoolView.jsx
@@ -2,10 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateSchoolStats } from "./schoolReducer";
 import { useEffect } from "react";
 import { NavLink } from "react-router";
+import { fetchStudents } from "../students/studentReducer";
 
 const SchoolView = () => {
   const dispatch = useDispatch();
-  const students = useSelector((state) => state.students.students);
+  const { students, status, error } = useSelector((state) => state.students);
   const school = useSelector((state) => state.school.school);
   const totalNumberOfStudents = students.length;
   const avgAttendance =
@@ -42,31 +43,44 @@ const SchoolView = () => {
     );
   }, [dispatch, totalNumberOfStudents, avgAttendance, avgMarks, topperStudent]);
 
+  useEffect(() => {
+    dispatch(fetchStudents());
+  }, [dispatch]);
+
   return (
     <main className="container py-4">
       <h1>School View </h1>
-      <h5>Total Students: {school && school.totalNumberOfStudents}</h5>
-      <h5>
-        Average Attendance:{" "}
-        {school && school.avgAttendance
-          ? school.avgAttendance.toFixed(2)
-          : "No students found."}{" "}
-      </h5>
-      <h5>
-        Average Marks:{" "}
-        {school && school.avgMarks
-          ? school.avgMarks.toFixed(2)
-          : "No students found."}
-      </h5>
-      <h5>
-        Topper Student:{" "}
-        {school && school.topperStudent
-          ? school.topperStudent
-          : "No student found."}
-      </h5>
+
+      {status === "Loading" ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <div>
+          <h5>Total Students: {school && school.totalNumberOfStudents}</h5>
+          <h5>
+            Average Attendance:
+            {school && school.avgAttendance
+              ? school.avgAttendance.toFixed(2)
+              : "No students found."}
+          </h5>
+          <h5>
+            Average Marks:
+            {school && school.avgMarks
+              ? school.avgMarks.toFixed(2)
+              : "No students found."}
+          </h5>
+          <h5>
+            Topper Student:
+            {school && school.topperStudent
+              ? school.topperStudent
+              : "No student found."}
+          </h5>
+        </div>
+      )}
 
       <NavLink to="teachers/addTeacher" className="btn btn-primary my-4">
-        Add Teacher{" "}
+        Add Teacher
       </NavLink>
     </main>
   );
